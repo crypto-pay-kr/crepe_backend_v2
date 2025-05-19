8f2f6eb65315053c6a2aa749f5498fdfd87c3c62
yunsy1103 <yunsy1103@naver.com>
2025-05-19 17:50:17 +0900

    feat: 내가 가입한 상품 목록 조회 api

---
+++ src/main/java/dev/crepe/domain/core/subscribe/model/dto/response/SubscribeResponseDto.java
@@ -0,0 +1,39 @@
+package dev.crepe.domain.core.subscribe.model.dto.response;
+
+import dev.crepe.domain.core.subscribe.model.SubscribeStatus;
+import dev.crepe.domain.core.subscribe.model.entity.Subscribe;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+public class SubscribeResponseDto {
+    private Long id; // 가입 ID
+    private Long bankTokenId; // 은행 토큰 id
+    private String productName; // 상품 이름
+    private SubscribeStatus status; // ACTIVE / EXPIRED
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private BigDecimal balance; // 현재 잔액
+    private float baseInterestRate; // 기본 이자율
+    private String appliedPreferentialRates; // 적용된 우대 금리 설명 (선택)
+    private String productType;
+
+    public static SubscribeResponseDto from(Subscribe s) {
+        return new SubscribeResponseDto(
+                s.getId(),
+                s.getProduct().getBankToken().getId(),
+                s.getProduct().getProductName(),
+                s.getStatus(),
+                s.getSubscribeDate().toLocalDate(),
+                s.getExpiredDate().toLocalDate(),
+                s.getBalance(),
+                s.getInterestRate(),
+                s.getAppliedPreferentialRates(),
+                s.getProduct().getType().name()
+        );
+    }
+}
