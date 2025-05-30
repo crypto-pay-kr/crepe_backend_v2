c9f470d7a1986b88ed175aaca9da8e6a84b5781b
bonun00 <lifesciences00@naver.com>
2025-05-30 17:14:35 +0900

    feat:출금 에러처리

--- src/main/java/dev/crepe/domain/core/transfer/service/Impl/WithdrawServiceImpl.java
+++ src/main/java/dev/crepe/domain/core/transfer/service/Impl/WithdrawServiceImpl.java
@@ -4,9 +4,6 @@ import dev.crepe.domain.core.account.exception.AccountNotFoundException;
 import dev.crepe.domain.core.account.model.AddressRegistryStatus;
 import dev.crepe.domain.core.account.model.entity.Account;
 import dev.crepe.domain.core.account.repository.AccountRepository;
-import dev.crepe.domain.core.account.exception.AccountNotRegisteredException;
-import dev.crepe.domain.core.account.exception.NotEnoughAmountException;
-import dev.crepe.domain.core.transfer.exception.WithdrawRequestFailedException;
 import dev.crepe.domain.core.transfer.model.dto.requset.GetWithdrawRequest;
 import dev.crepe.domain.core.transfer.model.dto.response.GetWithdrawResponse;
 import dev.crepe.domain.core.transfer.service.WithdrawService;
@@ -17,32 +14,35 @@ import dev.crepe.domain.core.util.history.business.model.TransactionType;
 import dev.crepe.domain.core.util.history.business.model.entity.TransactionHistory;
 import dev.crepe.domain.core.util.history.business.repository.TransactionHistoryRepository;
 import dev.crepe.domain.core.util.upbit.Service.UpbitWithdrawService;
+import dev.crepe.global.error.exception.ExceptionDbService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class WithdrawServiceImpl implements WithdrawService {
 
     private final TransactionHistoryRepository transactionHistoryRepository;
     private final AccountRepository accountRepository;
     private final UpbitWithdrawService upbitWithdrawService;
     private final CoinRepository coinRepository;
-
+    private final ExceptionDbService exceptionDbService;
 
     @Transactional
     @Override
     public void requestWithdraw(GetWithdrawRequest request,String email) {
-
+        log.info("출금 요청 처리 시작: {}", email);
         // 1.계좌 조회
         Account account = accountRepository.findByActor_EmailAndCoin_Currency(email, request.getCurrency())
-                .orElseThrow(() -> new AccountNotFoundException(email));
+                .orElseThrow(() -> exceptionDbService.getException("ACCOUNT_001"));
 
         // 2. 주소 등록 여부 확인
         if (account.getAddressRegistryStatus() != AddressRegistryStatus.ACTIVE) {
-            throw new AccountNotRegisteredException();
+           throw exceptionDbService.getException("ACCOUNT_005");
         }
 
         // 3. 코인 정보 확인
@@ -60,8 +60,9 @@ public class WithdrawServiceImpl implements WithdrawService {
         );
 
         if (response == null || response.getUuid() == null) {
-            throw new WithdrawRequestFailedException();
+            throw exceptionDbService.getException("WITHDRAW_001");
         }
+
 
         // 5. 거래 내역 저장 (공통)
         TransactionHistory history = TransactionHistory.builder()
@@ -77,7 +78,7 @@ public class WithdrawServiceImpl implements WithdrawService {
 
     private void validateSufficientBalance(Account account, BigDecimal requestAmount) {
         if (account.getBalance().compareTo(requestAmount) < 0) {
-            throw new NotEnoughAmountException(account.getCoin().getCurrency());
+        throw exceptionDbService.getException("ACCOUNT_006");
         }
     }
 
